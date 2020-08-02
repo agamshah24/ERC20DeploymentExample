f24885d316fef2120f8fa0430d5ac182b6360bd3
agam.udemy <agam.udemy@gmail.com>
2020-08-02 20:55:16 +0530

    initial commit

---
+++ contracts/SampleToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.6.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SampleToken is ERC20 {
+
+    constructor(string memory _name, string memory _symbol, uint256 initialSupply) public ERC20(_name, _symbol) {
+
+        require(initialSupply > 0, "INITIAL_SUPPLY has to be greater than 0");
+
+        _mint(msg.sender, initialSupply);
+    }
+
+
+}
